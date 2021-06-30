e52ff3c6a53374b07b62922a4ff46828b1a80790
ygorrodriguesmeli <ygor.rodrigues@mercadolivre.com>
2021-06-30 16:33:51 -0300

    Codigo morse now in request body

--- codigomorse/src/main/java/com/meli/codigomorse/CodigomorseController.java
+++ codigomorse/src/main/java/com/meli/codigomorse/CodigomorseController.java
@@ -2,6 +2,7 @@ package com.meli.codigomorse;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -16,8 +17,8 @@ public class CodigomorseController {
     private static final List<String> CHARACTERS = Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L",
             "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
 
-    @GetMapping(path = "/{codigo}")
-    public String conversaoCodigoMorse(@PathVariable String codigo) {
+    @GetMapping(path = "/morse")
+    public String conversaoCodigoMorse(@RequestBody String codigo) {
         String[] morseWords = codigo.split("\\s{3}");
         StringBuilder retorno = new StringBuilder();
         for(String morseWord : morseWords) {
